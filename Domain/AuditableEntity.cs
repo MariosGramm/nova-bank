5ec06f144352e03eaf5696ac8b385529d7610915
MariosGrammPC <grammatopoulosmarios@gmail.com>
2026-01-05 22:48:37 +0200

    Added AuditableEntity class

---
+++ Domain/AuditableEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    internal class AuditableEntity
+    {
+        public DateTime CreatedAt { get; set; } 
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime DeletedAt { get; set; }
+    }
+}
