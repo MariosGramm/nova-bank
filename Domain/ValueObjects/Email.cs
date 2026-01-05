f5698b490edcbbf632d02299eea674783aa8d0ec
MariosGrammPC <grammatopoulosmarios@gmail.com>
2026-01-05 23:40:21 +0200

    Added Email value object

---
+++ Domain/ValueObjects/Email.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace Domain.ValueObjects
+{
+    public sealed record Email 
+    {
+        public string? _value { get; }
+
+        public Email (string value)
+        {
+            if (string.IsNullOrEmpty(value)) throw new ArgumentNullException("Email cannot be empty");
+
+            value = value.Trim();
+
+            if (!isEmailValid(value))
+            {
+                throw new ArgumentException("Invalid email insertion");
+            }
+
+            _value = value;
+        }
+
+        private static bool isEmailValid(string value)
+        {
+            string regEx = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$\r\n";
+
+            return Regex.IsMatch(value, regEx);
+        }
+
+        public override string ToString()
+        {
+            #pragma warning disable
+            return _value;
+        }
+    }
+}
