2cf5c6337e0f050101284ced5ab17952ede7f5da
MariosGrammPC <grammatopoulosmarios@gmail.com>
2026-01-06 14:04:30 +0200

    Fixed spelling in FirstName and LastName classes

--- Domain/ValueObjects/LastName.cs
+++ Domain/ValueObjects/LastName.cs
@@ -3,25 +3,25 @@ using System.Text.RegularExpressions;
 
 namespace Domain.ValueObjects
 {
-    public sealed record FirstName
+    public sealed record LastName
     {
         public string? _value { get; }
 
-        public FirstName(string value)
+        public LastName(string value)
         {
-            if (string.IsNullOrWhiteSpace(value)) throw new ArgumentNullException("First name cannot be empty");
+            if (string.IsNullOrWhiteSpace(value)) throw new ArgumentNullException("Last name cannot be empty");
 
             value = value.Trim();
 
-            if (!isFirstNameValid(value))
+            if (!isLastNameValid(value))
             {
-                throw new ArgumentException("Invalid first name insertion");
+                throw new ArgumentException("Invalid last name insertion");
             }
 
             _value = value;
         }
 
-        private static bool isFirstNameValid(string value)
+        private static bool isLastNameValid(string value)
         {
             string regEx = "^[A - Za - z]+([ '-][A-Za-z]+)*$";
 
