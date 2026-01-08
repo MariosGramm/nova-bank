b6c263290804491522408994ad6f27990b8ebf26
MariosGrammPC <grammatopoulosmarios@gmail.com>
2026-01-08 22:46:22 +0200

    Added FirstName and LastName as common string fields in User entity , not VOs

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -7,6 +7,8 @@ namespace Domain.Entities
     internal class User : AuditableEntity
     {
         public int Id { get; set; }
-        public 
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty ;
+
     }
 }
