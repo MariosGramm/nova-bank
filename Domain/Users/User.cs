ce5a81c9719300d944acb009fae1bdec6aec28be
MariosGrammPC <grammatopoulosmarios@gmail.com>
2026-01-09 01:01:37 +0200

    Added Users aggregate consisting of User Aggregate Root and Email value object

--- Domain/Users/User.cs
+++ Domain/Users/User.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿using Domain.Common;
+using Domain.Users.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Domain.Entities
+namespace Domain.Users
 {
     internal class User : AuditableEntity
     {
         public int Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty ;
+        public string LastName { get; set; } = string.Empty;
+        public Email Email { get; set; }
 
     }
 }
---
+++ Domain/Users/ValueObjects/Email.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Domain.Users.ValueObjects
+{
+    public sealed record Email 
+    {
+        public string Value { get; }
+
+        private Email(string value) 
+        {
+            Value = value;      // validated value 
+        }
+
+        public static Email Create(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException("Email cannot be empty");
+
+            if (!isEmailValid(value)) throw new ArgumentException("Invalid email format");
+            
+            return new Email(value);
+        }
+
+
+        private static bool isEmailValid(string value)
+        {
+            string regEx = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$\r\n";
+
+            return Regex.IsMatch(value, regEx);
+        }
+
+        public override string ToString()
+        {
+            #pragma warning disable
+            return Value;
+        }
+    }
+}
